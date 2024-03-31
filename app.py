255791d081d7495ee13955c3ee3884865d463c9b
Carlos <JoseCruz96@hotmail.com>
2024-03-30 18:44:40 -0600

    scatter check box button

--- app.py
+++ app.py
@@ -7,13 +7,26 @@ car_data = pd.read_csv("../vehicles_us.csv")   #Leer datos
 st.header("Anuncios de venta de coches")
 
 hist_button = st.button('Construir histograma') #Crear boton
+scatter_checkbox = st.checkbox("Construir diagrama de dispersion")
 
 if hist_button: # al hacer clic en el botón
-            # escribir un mensaje
-            st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-            
-            # crear un histograma
-            fig = px.histogram(car_data, x="odometer")
+    # escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+if scatter_checkbox: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write("Creación de un diagrama de dispersión para el conjunto de datos de anuncios de ventas de coches")
+
+    # Crear un diagrama de dispersión
+    fig = px.scatter(x="model" ,y="cilinder")
+
+    # mostrar un gráfico Ploty interactivo
+    st.plotly_chart(fig,use_container_width=True)
+
         
-            # mostrar un gráfico Plotly interactivo
-            st.plotly_chart(fig, use_container_width=True)
