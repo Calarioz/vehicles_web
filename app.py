0bca44cb98711a1464a7ad29306d0197510c3fb6
Carlos <JoseCruz96@hotmail.com>
2024-03-30 18:51:22 -0600

    initial commit

--- app.py
+++ app.py
@@ -14,19 +14,19 @@ if hist_button: # al hacer clic en el botón
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
     
     # crear un histograma
-    fig = px.histogram(car_data, x="odometer")
+    fig1 = px.histogram(car_data, x="odometer")
 
     # mostrar un gráfico Plotly interactivo
-    st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig1, use_container_width=True)
 
 if scatter_checkbox: # al hacer clic en el botón
     # escribir un mensaje
     st.write("Creación de un diagrama de dispersión para el conjunto de datos de anuncios de ventas de coches")
 
     # Crear un diagrama de dispersión
-    fig = px.scatter(car_data, x="model" ,y="cylinders")
+    fig2 = px.scatter(car_data, x="model" ,y="cylinders")
 
     # mostrar un gráfico Ploty interactivo
-    st.plotly_chart(fig,use_container_width=True)
+    st.plotly_chart(fig2,use_container_width=True)
 
         
